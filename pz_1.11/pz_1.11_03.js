17fe8cd9708e08c152aa053a3cc98a1f59edbf9c
Alex <lolmagexe@gmail.com>
2022-02-08 12:39:35 +0300

    add 03

--- pz_1.11/pz_1.11_03.js
+++ pz_1.11/pz_1.11_03.js
@@ -1,16 +1,7 @@
 let EXAMPLE = [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]
 
-let GetArray = (arr) => {
-    let res = []
-    for(let i = 0; i < arr.length; i ++) {
-        let element = arr[i]
-
-        res.push(...element)
-    }
-
-    res.sort()
-
-    return res
-} 
+let GetArray = (arr = []) => {
+    return arr.reduce((accum, item) =>  accum.concat(...item)).sort()
+}
 
 console.log(GetArray(EXAMPLE));
